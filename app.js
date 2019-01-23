d056234759304bee83310bf4f470d29bada6e05f
Michelle Ho <michellemmh@gmail.com>
2019-01-23 15:37:14 -0500

    changing to mapbox

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-let myMap = L.map('mapid').setView([40.6946, -73.949], 12);
+mapboxgl.accessToken = mapbox_key
 
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-  maxZoom: 18,
-  id: 'mapbox.streets',
-  accessToken: 'pk.eyJ1IjoiZGFuaWVsYnVydG9uIiwiYSI6ImNqcXpnajc4MTAybnI0NG1tOXo5Nm55dWUifQ.DPEZeklc_mPhpt_2aXz4SA'
-  }).addTo(myMap);
+var myMap = new mapboxgl.Map({
+  container: 'mapid',
+  style: 'mapbox://styles/mapbox/streets-v9',
+  center: [-73.949,40.6946],
+  zoom: 10
+});
 
 boroughInput = document.getElementById("boro");
 addressInput = document.getElementById("address");
@@ -29,16 +29,19 @@ async function geocodeAddress(address, borough) {
   let query = `https://geosearch.planninglabs.nyc/v1/search?text=${address}, ${borough}`
   const response = await fetch(query);
   const myJson = await response.json();
-  return [myJson['features'][0]['geometry']['coordinates'][1],
-          myJson['features'][0]['geometry']['coordinates'][0]];
+  return [myJson['features'][0]['geometry']['coordinates'][0],
+          myJson['features'][0]['geometry']['coordinates'][1]];
 }
 
 
 function addMarker(long, lat) {
-  let marker = L.marker([long, lat]).addTo(myMap);
-  return marker;
+  var marker = new mapboxgl.Marker()
+  .setLngLat([long, lat])
+  .addTo(myMap);
   }
 
 function geocodeAndDraw(address, borough){
-  return geocodeAddress(address, borough).then(latLong => {addMarker(latLong[1], latLong[0])})
+  return geocodeAddress(address, borough).then(latLong => {addMarker(latLong[0], latLong[1])})
 }
+
+// function getIsochrones()
